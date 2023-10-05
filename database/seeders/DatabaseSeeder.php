62234bdaa96193ea2e890fbd9d14554f6d7e6a76
FerdiSus <ferdisusanto006@gmail.com>
2023-10-05 11:10:00 +0700

    baru

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,12 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Buku;
+use App\Models\Peminjaman;
+use App\Models\Anggota;
+use App\Models\Rak;
+use App\Models\Pengembalian;
+use App\Models\Petugas;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +18,123 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Anggota::create([
+            "kode_anggota"  => '123456789',
+            "nama_anggota" => 'Ferdi Susanto',
+            "jenis_kelamin" => 'L',
+            "jurusan_anggota" => 'RPL',
+            "no_telp" => '081905883680',
+            "alamat"  =>'Ntar di serlok'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Anggota::create([
+            "kode_anggota"  => '123456780',
+            "nama_anggota" => 'Funky Alamsyah',
+            "jenis_kelamin" => 'P',
+            "jurusan_anggota" => 'BDP',
+            "no_telp" => '081111111',
+            "alamat"  =>'Disana'
+        ]);
+
+        Petugas::create([
+            "nama_petugas" => 'Babeh',
+            "jabatan_petugas" => 'security',
+            "no_telp_petugas" => '01010101010',
+            "alamat_petugas" => 'disamping sekolah'
+        ]);
+
+        Petugas::create([
+            "nama_petugas" => 'Agung',
+            "jabatan_petugas" => 'Admin',
+            "no_telp_petugas" => '012220101010',
+            "alamat_petugas" => 'disamping sekolah'
+        ]);
+        Petugas::create([
+            "nama_petugas" => 'Three',
+            "jabatan_petugas" => 'Staf TU',
+            "no_telp_petugas" => '010122210',
+            "alamat_petugas" => 'disamping sekolah'
+        ]);
+
+        Buku::create([
+            "kode_buku" => '13345',
+            "judul_buku" => 'Kisah 25 Nabi dan Rasul',
+            "Penulis_buku" => 'Andika Cahyadi',
+            "penerbit_buku" => 'Gramedia',
+            "tahun_penerbit" => '2023',
+            "stok" => 32,
+        ]);
+        Buku::create([
+            "kode_buku" => '14345',
+            "judul_buku" => 'Mengejar yang Lari',
+            "Penulis_buku" => 'Laskar Pendongeng',
+            "penerbit_buku" => 'Gramedia',
+            "tahun_penerbit" => '2012',
+            "stok" => 42
+        ]);
+        Buku::create([
+            "kode_buku" => '15345',
+            "judul_buku" => 'Perpindahan Negara Asia',
+            "Penulis_buku" => 'Hitler',
+            "penerbit_buku" => 'Gramedia',
+            "tahun_penerbit" => '2019',
+            "stok" => 25
+        ]);
+
+        Rak::create([
+            "nama_rak" => "Agama",
+            "lokasi_rak" => "lt-3",
+            "buku_id" => 1,
+        ]);
+        Rak::create([
+            "nama_rak" => "Politik",
+            "lokasi_rak" => "lt-2",
+            "buku_id" => 3,
+        ]);
+        Rak::create([
+            "nama_rak" => "Motivasi",
+            "lokasi_rak" => "lt-1",
+            "buku_id" => 2,
+        ]);
+       
+        Peminjaman::create([
+            "tanggal_pinjam" => "2023-10-02",
+            "tanggal_kembali" => "2023-10-07",
+            "buku_id" => 1,
+            "anggota_id" => 1,
+            "petugas_id" => 2,
+        ]);
+
+        Peminjaman::create([
+            "tanggal_pinjam" => "2023-10-02",
+            "tanggal_kembali" => "2023-10-07",
+            "buku_id" => 2,
+            "anggota_id" => 1,
+            "petugas_id" => 1,
+        ]);
+
+        Peminjaman::create([
+            "tanggal_pinjam" => "2023-10-02",
+            "tanggal_kembali" => "2023-10-07",
+            "buku_id" => 3,
+            "anggota_id" => 2,
+            "petugas_id" => 3,
+        ]);
+
+        Pengembalian::create([
+            "tanggal_pengembalian" => "2023-10-08",
+            "denda" => 35000,
+            "buku_id" => 1,
+            "anggota_id" => 1,
+            "petugas_id" => 1
+         ]);
+   
+         Pengembalian::create([
+           "tanggal_pengembalian" => "2023-10-04",
+           "denda" => 0,
+           "buku_id" => 2,
+           "anggota_id" => 2,
+           "petugas_id" => 2
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BukuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('/buku')->group(function(){
+    Route::get('/', [BukuController::class,'index']);
+    Route::post('/create',[BukuController::class,'create']);
+    Route::put('/update',[BukuController::class,'update']);
+    Route::delete('/delete',[BukuController::class,'delete']);
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
